22db3e61af86b0a58665432f53defbf431fd25f7
Desmond To <e0550619@u.nus.edu>
2020-12-29 22:05:29 +0800

    Added style modifications to table, detail, CRUD pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :to_dos
   # get 'home/index'
   get 'home/about'
-  root 'home#index'
+  root 'to_dos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
